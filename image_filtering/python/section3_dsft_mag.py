d22879d69e16cc73bd312744da3f2d3ce943970c
Joseph Kulikowski <76257372+joekulikowski15@users.noreply.github.com>
2025-01-17 18:38:10 -0600

    bugs in plotting code

--- image_filtering/python/section3_dsft_mag.py
+++ image_filtering/python/section3_dsft_mag.py
@@ -50,7 +50,7 @@ conds = np.logical_or(np.abs(muMesh)>np.pi,np.abs(nuMesh)>np.pi)
 HHplot_negpi_pi[conds] = np.nan
 fig1, ax1 = plt.subplots(subplot_kw={"projection": "3d"})
 surf = ax1.plot_surface(muMesh, nuMesh, np.abs(HHplot_negpi_pi),
-                        rstride=1, cstride=1,
+                        rstride=2, cstride=2,
                         facecolors = cm.coolwarm(np.abs(HHplot_negpi_pi)))
 ax1.set_xlim([-np.pi, np.pi])
 ax1.set_ylim([-np.pi, np.pi])
@@ -59,7 +59,7 @@ ax1.set_zlabel(r"$|H(e^{j \mu}, e^{j \nu})|$")
 ax1.set_xlabel(r"$\mu$")
 ax1.set_ylabel(r"$\nu$")
 fig1.set_size_inches(8, 5)
-fig1.savefig(os.path.join(this_dir,"..","images","DSFTmag3D.jpg"), dpi=2000)
+fig1.savefig(os.path.join(this_dir,"..","images","DSFTmag3D.jpg"), dpi=500)
 
 plt.rcParams["figure.figsize"] = (5,8)
 HHplot_negpi_pi = np.copy(HH)
@@ -67,7 +67,7 @@ conds = np.logical_or(np.abs(muMesh)>3*np.pi,np.abs(nuMesh)>3*np.pi)
 HHplot_negpi_pi[conds] = np.nan
 fig2, ax2 = plt.subplots(subplot_kw={"projection": "3d"})
 surf = ax2.plot_surface(muMesh, nuMesh, np.abs(HHplot_negpi_pi),
-                        rstride=1, cstride=1,
+                        rstride=2, cstride=2,
                         facecolors = cm.coolwarm(np.abs(HHplot_negpi_pi)))
 ax2.set_xlim([-3*np.pi, 3*np.pi])
 ax2.set_ylim([-3*np.pi, 3*np.pi])
@@ -76,17 +76,7 @@ ax2.set_zlabel(r"$|H(e^{j \mu}, e^{j \nu})|$")
 ax2.set_xlabel(r"$\mu$")
 ax2.set_ylabel(r"$\nu$")
 fig2.set_size_inches(8, 5)
-fig2.savefig(os.path.join(this_dir,"..","images","DSFTmag3D_5pi.jpg"), dpi=2000)
-
-new_shape = (100,100)
-fig2, ax2 = plt.subplots()
-m = cm.ScalarMappable(cmap='coolwarm')
-m.set_array(np.abs(HHplot_negpi_pi))
-ax2.imshow(np.abs(HHplot_negpi_pi[~conds].reshape(new_shape)), cmap='coolwarm')
-cbar = fig2.colorbar(m)
-cbar.set_label(r"$|H(e^{j \mu}, e^{j \nu})|$")
-ax2.set_xticklabels(nuMesh[~conds].reshape(new_shape)[::10,1])
-plt.show()
+fig2.savefig(os.path.join(this_dir,"..","images","DSFTmag3D_3pi.jpg"), dpi=500)
 
 # Finishing out section 3...
 img03 = Image.open(os.path.join(this_dir, "..", "images", "img03.tif"))
